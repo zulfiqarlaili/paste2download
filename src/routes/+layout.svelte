1f24aea5dc3d8dd4521d8fae770ba9f081f01631
zulfiqarlaili <zulfiqarlaili@gmail.com>
2024-02-22 22:28:19 +0800

    1st commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import '../app.pcss';
+	import { ModeWatcher } from 'mode-watcher';
+	import { Button } from '$lib/components/ui/button';
+	import Sun from 'lucide-svelte/icons/sun';
+	import Moon from 'lucide-svelte/icons/moon';
+	import { toggleMode } from 'mode-watcher';
+	import AOS from 'aos';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		AOS.init();
+	});
+</script>
+
+<div class="absolute right-0 top-0">
+	<Button on:click={toggleMode} variant="outline" size="icon">
+		<Sun
+			class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+		/>
+		<Moon
+			class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+		/>
+		<span class="sr-only">Toggle theme</span>
+	</Button>
+</div>
+<ModeWatcher />
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<div class="mt-20 flex h-screen justify-center">
+	<div class="mx-auto max-w-screen-xl px-4 py-8 text-center lg:px-12 lg:py-16">
+		<h1
+			class="leading-tighter mb-4 text-5xl font-extrabold tracking-tighter md:text-6xl"
+			data-aos="zoom-y-out"
+		>
+			Easy video downloads
+			<br />
+			<span
+				class="block bg-gradient-to-r from-blue-500 to-teal-400 bg-clip-text text-transparent"
+				data-aos="zoom-y-out">no watermark</span
+			>
+		</h1>
+		<p
+			class="mb-8 text-lg text-muted-foreground sm:px-16 lg:text-xl xl:px-48"
+			data-aos="zoom-y-out"
+			data-aos-delay="300"
+		>
+			Download videos effortlessly, remove watermarks, and share instantly. Welcome to
+			Paste2Download
+		</p>
+		<Button class="w-36" data-aos="zoom-y-out" data-aos-delay="300">Get Started</Button>
+	</div>
+</div>
