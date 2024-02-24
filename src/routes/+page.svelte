56f618e3dabb921b6c19cd97ef67be92fc253c69
zulfiqarlaili <zulfiqarlaili@gmail.com>
2024-02-25 03:08:55 +0800

    feat: add product roadmap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
 	import { Separator } from '$lib/components/ui/separator';
+	import { Check } from 'lucide-svelte';
 </script>
 
 <div class="mt-20 flex min-h-screen justify-center">
@@ -28,10 +29,10 @@
 			class="w-36 bg-gradient-to-r from-blue-500 to-teal-400"
 			data-aos="fade-up"
 			data-aos-delay="300"
-			href='/paste-link'>Get Started</Button
+			href="/paste-link">Get Started</Button
 		>
 
-		<Separator class="my-8" />
+		<Separator class="my-16" />
 		<div data-aos="fade-up" data-aos-delay="600">
 			<h2
 				class="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
@@ -39,10 +40,77 @@
 				How does it work
 			</h2>
 			<p class="mx-auto max-w-[30rem] text-sm text-muted-foreground">
-				Paste a video link to get started; mostly TikTok have watermark in their videos that we want
-				to remove; paste and let the magic happen
+				Paste a video link to get started; mostly <b>TikTok</b> have watermark in their videos that we
+				want to remove; paste and let the magic happen
 			</p>
+		</div>
+		<Separator class="my-16" />
+		<div class="mx-auto max-w-[30rem]" data-aos="fade-up" data-aos-delay="900">
+			<h2
+				class="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+			>
+				Product roadmap
+			</h2>
+
+			<ol class="relative mt-8 border-s border-gray-200 text-left dark:border-gray-700">
+				<li class="mb-10 ms-6">
+					<span
+						class="absolute -start-3 flex h-6 w-6 items-center justify-center rounded-full bg-green-400 ring-8 ring-white dark:bg-green-700 dark:ring-gray-900"
+					>
+						<Check class="h-4 w-4" />
+					</span>
+					<h3 class="mb-1 flex items-center text-lg font-semibold text-gray-900 dark:text-white">
+						Easy TikTok Video Downloader
+					</h3>
+					<p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+						Simply paste the link of your favorite TikTok video and download it hassle-free,
+						completely watermark-free.
+					</p>
+				</li>
+				<li class="mb-10 ms-6">
+					<span
+						class="absolute -start-3 flex h-6 w-6 items-center justify-center rounded-full bg-blue-100 ring-8 ring-white dark:bg-slate-700 dark:ring-gray-900"
+					>
+					</span>
+					<h3 class="mb-1 text-lg font-semibold text-gray-400 dark:text-gray-500">
+						<!-- <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white"> -->
+						Integration with X (formerly Twitter)
+					</h3>
+					<p class="font-normal text-gray-400 text-base dark:text-gray-500">
+						<!-- <p class="text-base font-normal text-gray-500 dark:text-gray-400"> -->
+						Instantly share your video link to your X account. We'll download and remove the
+						watermark for you, posting it on your X instantly. No need to download it yourself.
+					</p>
+				</li>
+				<li class="mb-10 ms-6">
+					<span
+						class="absolute -start-3 flex h-6 w-6 items-center justify-center rounded-full bg-blue-100 ring-8 ring-white dark:bg-slate-700 dark:ring-gray-900"
+					>
+					</span>
+					<h3 class="mb-1 text-lg font-semibold text-gray-400 dark:text-gray-500">
+						<!-- <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white"> -->
+						Integration with Facebook
+					</h3>
+					<p class="font-normal text-gray-400 text-base dark:text-gray-500">
+						<!-- <p class="text-base font-normal text-gray-500 dark:text-gray-400"> -->
+						Coming soon
+					</p>
+				</li>
+				<li class="mb-10 ms-6">
+					<span
+						class="absolute -start-3 flex h-6 w-6 items-center justify-center rounded-full bg-blue-100 ring-8 ring-white dark:bg-slate-700 dark:ring-gray-900"
+					>
+					</span>
+					<h3 class="mb-1 text-lg font-semibold text-gray-400 dark:text-gray-500">
+						<!-- <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white"> -->
+						Integration with Shopee affiliate
+					</h3>
+					<p class="font-normal text-gray-400 text-base dark:text-gray-500">
+						<!-- <p class="text-base font-normal text-gray-500 dark:text-gray-400"> -->
+						Coming soon
+					</p>
+				</li>
+			</ol>
 		</div>
 	</div>
 </div>
-
